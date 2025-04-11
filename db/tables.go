9e597de6f93d01901018fc660d498c9da9d6f482
salvis <sjansons2003@gmail.com>
2025-04-11 12:04:28 +0300

    add: db tables

--- db/tables.go
+++ db/tables.go
@@ -7,17 +7,62 @@ import (
 )
 
 func createTables(db *sql.DB) error {
-	_, err := db.Exec(`
-		CREATE TABLE IF NOT EXISTS users (
+	queries := []string{
+		`CREATE TABLE IF NOT EXISTS users (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			email TEXT NOT NULL UNIQUE,
 			password TEXT NOT NULL
-		);
-	`)
-	if err != nil {
-		return fmt.Errorf("error creating tables: %v", err)
+		);`,
+		`CREATE TABLE IF NOT EXISTS training_plans (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			title TEXT NOT NULL,
+			user_id INT NOT NULL,
+			FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+		);`,
+		`CREATE TABLE IF NOT EXISTS workout_days (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			title TEXT NOT NULL,
+			user_id INT NOT NULL,
+			day_of_week TEXT NOT NULL,  -- Store days like 'Monday', 'Tuesday'
+			training_plan_id INT NOT NULL,
+			FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+			FOREIGN KEY (training_plan_id) REFERENCES training_plans(id) ON DELETE CASCADE
+		);`,
+		`CREATE TABLE IF NOT EXISTS exercises (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			title TEXT NOT NULL,
+			user_id INT NOT NULL,
+			workout_day_id INT NOT NULL,
+			FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+			FOREIGN KEY (workout_day_id) REFERENCES workout_days(id) ON DELETE CASCADE
+		);`,
+		`CREATE TABLE IF NOT EXISTS sets (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			weight INT NOT NULL,
+			reps INT NOT NULL,
+			user_id INT NOT NULL,
+			exercise_id INT NOT NULL,
+			FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+			FOREIGN KEY (exercise_id) REFERENCES exercises(id) ON DELETE CASCADE
+		);`,
 	}
 
-	log.Println("Tables created or already exist.")
+	tx, err := db.Begin()
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %w", err)
+	}
+
+	for _, query := range queries {
+		if _, err := tx.Exec(query); err != nil {
+			tx.Rollback()
+			return fmt.Errorf("failed to execute query: %s, error: %w", query, err)
+		}
+	}
+
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("failed to commit transaction: %w", err)
+	}
+
+	log.Println("Tables successfully created or already exist.")
 	return nil
 }
